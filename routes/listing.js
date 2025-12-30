a19b0e22b41ecfae13a310a90f303c7278e365c0
aryankhanf22 <aryankhanf22@gmail.com>
2025-12-30 19:12:38 +0500

    Add route listing file

---
+++ routes/listing.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapasync.js");
+const { isLoggedin, isOwner, validatelisting } = require("../middleware.js");
+const listingController = require("../controllers/listing.js");
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedin, validatelisting, wrapAsync(listingController.create));
+// new route
+router.get("/new", isLoggedin, listingController.renderNewform);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.show))
+  .put(isLoggedin, isOwner, validatelisting, wrapAsync(listingController.edit)).delete( isLoggedin, isOwner, wrapAsync(listingController.delete))
+
+// edit route
+router.get(
+  "/:id/edit",
+  isLoggedin,
+  isOwner,
+  wrapAsync(listingController.editForm)
+);
+module.exports = router;
