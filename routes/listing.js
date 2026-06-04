d98f1c5ab0ab510c294c009bed6e8cdb58008401
aryankhanf22 <aryankhanf22@gmail.com>
2026-06-04 17:59:05 +0500

    Add listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -4,24 +4,28 @@ const listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapasync.js");
 const { isLoggedin, isOwner, validatelisting } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(isLoggedin, validatelisting, wrapAsync(listingController.create));
+  .post(isLoggedin,upload.single("listing[image]"), validatelisting,wrapAsync(listingController.create));
 // new route
 router.get("/new", isLoggedin, listingController.renderNewform);
 
 router
   .route("/:id")
   .get(wrapAsync(listingController.show))
-  .put(isLoggedin, isOwner, validatelisting, wrapAsync(listingController.edit)).delete( isLoggedin, isOwner, wrapAsync(listingController.delete))
+  .put(isLoggedin, isOwner, upload.single("listing[image]"),validatelisting, wrapAsync(listingController.edit))
+  .delete(isLoggedin, isOwner, wrapAsync(listingController.delete));
 
 // edit route
 router.get(
   "/:id/edit",
   isLoggedin,
   isOwner,
-  wrapAsync(listingController.editForm)
+  wrapAsync(listingController.editForm),
 );
 module.exports = router;
